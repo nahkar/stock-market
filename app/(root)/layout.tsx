50d0ab65ab8c13be3e82ffdd68a8e822dd61f67a
Belenov Dmitriy <dimabelenov@gmail.com>
2026-04-29 23:38:35 +0300

    refactor: Remove unused Home component from page.tsx

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return <main className="min-h-screen text-gray-400">
+		<div className="container py-10">
+			{children}
+		</div>
+	</main>;
+}
